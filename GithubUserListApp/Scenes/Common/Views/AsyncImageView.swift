67cb0b3635b3cbecd38c8b265f4d5a7dd7fa24f2
EdYuTo <edson.yudi.toma@gmail.com>
2024-01-21 17:55:22 -0300

    Add AsyncImageView

---
+++ GithubUserListApp/Scenes/Common/Views/AsyncImageView.swift
@@ -0,0 +1,94 @@
+//
+//  AsyncImageView.swift
+//  GithubUserListApp
+//
+//  Created by Edson Yudi Toma on 21/01/24.
+//
+
+import UIKit
+
+final class AsyncImageView: UIImageView {
+    // MARK: - Properties
+    let iconSize: CGSize
+    lazy var customViews = [loadingView, iconView]
+
+    // MARK: - Views
+    lazy var loadingView: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView(style: .gray)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    lazy var iconView: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    // MARK: - Life cycle
+    init(iconSize: CGSize) {
+        self.iconSize = iconSize
+        super.init(frame: .zero)
+        setupView()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Methods
+    func startLoading() {
+        updateMainView(with: loadingView)
+        loadingView.startAnimating()
+    }
+
+    func startDownload(url: String) {
+        startLoading()
+        ImageProvider.getImage(url: url) { uiImage in
+            DispatchQueue.main.async { [weak self] in
+                self?.iconView.image = uiImage
+                self?.updateMainView(with: self?.iconView)
+            }
+        }
+    }
+
+    private func updateMainView(with view: UIView?) {
+        guard let view = view else { return }
+        customViews.forEach { view in
+            view.isHidden = true
+        }
+        UIView.transition(
+            with: view, duration: 0.5,
+            options: .transitionCrossDissolve,
+            animations: {
+                view.isHidden = false
+            }
+        )
+        setNeedsLayout()
+        layoutIfNeeded()
+    }
+}
+
+// MARK: - ViewCodeProtocol
+extension AsyncImageView: ViewCodeProtocol {
+    func setupHierarchy() {
+        addSubview(loadingView)
+        addSubview(iconView)
+    }
+
+    func setupConstraints() {
+        widthAnchor.constraint(equalToConstant: iconSize.width).isActive = true
+
+        let heightConstraint = heightAnchor.constraint(equalToConstant: iconSize.height)
+        heightConstraint.priority = .defaultHigh
+        heightConstraint.isActive = true
+
+        customViews.forEach { view in
+            NSLayoutConstraint.activate([
+                view.widthAnchor.constraint(equalToConstant: iconSize.width),
+                view.heightAnchor.constraint(equalToConstant: iconSize.height)
+            ])
+        }
+    }
+}
--- GithubUserListApp/Scenes/UserListScene/Views/UserTableViewCell.swift
+++ GithubUserListApp/Scenes/UserListScene/Views/UserTableViewCell.swift
@@ -10,24 +10,11 @@ import UIKit
 final class UserTableViewCell: UITableViewCell {
     // MARK: - Properties
     static let reuseIdentifier = String(describing: UserTableViewCell.self)
-    let iconSize: CGFloat = Spacing.x64.value
+    let iconSize = Spacing.x64.value
 
     // MARK: - Views
-    lazy var avatar: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-
-    lazy var loadingView: UIActivityIndicatorView = {
-        let view = UIActivityIndicatorView(style: .gray)
-        view.startAnimating()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-
-    lazy var iconView: UIImageView = {
-        let view = UIImageView()
+    lazy var iconView: AsyncImageView = {
+        let view = AsyncImageView(iconSize: CGSize(width: iconSize, height: iconSize))
         view.layer.cornerRadius = iconSize / 2
         view.layer.masksToBounds = true
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -42,7 +29,7 @@ final class UserTableViewCell: UITableViewCell {
     }()
 
     lazy var mainStackView: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [avatar, userName])
+        let stack = UIStackView(arrangedSubviews: [iconView, userName])
         stack.axis = .horizontal
         stack.spacing = iconSize / 2
         stack.translatesAutoresizingMaskIntoConstraints = false
@@ -52,36 +39,14 @@ final class UserTableViewCell: UITableViewCell {
     // MARK: - Life cycle
     override func prepareForReuse() {
         super.prepareForReuse()
-        updateAvatar(with: loadingView)
-        loadingView.startAnimating()
+        iconView.startAnimating()
     }
 
     // MARK: Methods
     func setup(_ model: UserSuccess) {
         userName.text = model.userName
         setupView()
-        ImageProvider.getImage(url: model.avatarUrl) { uiImage in
-            DispatchQueue.main.async { [weak self] in
-                self?.iconView.image = uiImage
-                self?.updateAvatar(with: self?.iconView)
-            }
-        }
-    }
-
-    private func updateAvatar(with view: UIView?) {
-        guard let view = view else { return }
-        avatar.subviews.forEach { view in
-            view.isHidden = true
-        }
-        UIView.transition(
-            with: view, duration: 0.5,
-            options: .transitionCrossDissolve,
-            animations: {
-                view.isHidden = false
-            }
-        )
-        setNeedsLayout()
-        layoutIfNeeded()
+        iconView.startDownload(url: model.avatarUrl)
     }
 }
 
@@ -89,8 +54,6 @@ final class UserTableViewCell: UITableViewCell {
 extension UserTableViewCell: ViewCodeProtocol {
     func setupHierarchy() {
         addSubview(mainStackView)
-        avatar.addSubview(loadingView)
-        avatar.addSubview(iconView)
     }
 
     func setupConstraints() {
@@ -98,17 +61,7 @@ extension UserTableViewCell: ViewCodeProtocol {
             mainStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Spacing.x16.value),
             mainStackView.topAnchor.constraint(equalTo: topAnchor, constant: Spacing.x16.value),
             mainStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Spacing.x16.value),
-            mainStackView.bottomAnchor.constraint(lessThanOrEqualTo: bottomAnchor, constant: -Spacing.x16.value),
-
-            avatar.widthAnchor.constraint(equalToConstant: iconSize),
-            avatar.heightAnchor.constraint(equalTo: avatar.widthAnchor)
+            mainStackView.bottomAnchor.constraint(lessThanOrEqualTo: bottomAnchor, constant: -Spacing.x16.value)
         ])
-
-        avatar.subviews.forEach { view in
-            NSLayoutConstraint.activate([
-                view.widthAnchor.constraint(equalTo: avatar.widthAnchor),
-                view.heightAnchor.constraint(equalTo: avatar.heightAnchor)
-            ])
-        }
     }
 }
