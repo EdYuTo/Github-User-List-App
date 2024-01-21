551c101ca306c8c1575dade64a6a88a770f97df9
EdYuTo <edson.yudi.toma@gmail.com>
2024-01-21 20:00:04 -0300

    Move to commons

---
+++ GithubUserListApp/Scenes/Common/Views/ErrorTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  ErrorTableViewCell.swift
+//  GithubUserListApp
+//
+//  Created by Edson Yudi Toma on 20/01/24.
+//
+
+import UIKit
+
+final class ErrorTableViewCell: UITableViewCell {
+    // MARK: - Properties
+    static let reuseIdentifier = String(describing: ErrorTableViewCell.self)
+
+    // MARK: - Views
+    lazy var button: UIButton = {
+        let button = UIButton()
+        button.setTitle(Localizable.retryButtonTitle.localized, for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.isUserInteractionEnabled = false
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    // MARK: - Methods
+    func setup() {
+        setupView()
+    }
+}
+
+// MARK: - ViewCodeProtocol
+extension ErrorTableViewCell: ViewCodeProtocol {
+    func setupHierarchy() {
+        addSubview(button)
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
